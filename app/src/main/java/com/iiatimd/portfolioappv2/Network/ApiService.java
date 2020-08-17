7fccdedfa258877f0fd1745d95767daeb86c337f
Klaas Schoute <klaas_schoute@hotmail.com>
2020-08-18 00:00:42 +0200

    :sparkles: Api call met error handling

---
+++ app/src/main/java/com/iiatimd/portfolioappv2/Network/ApiService.java
@@ -0,0 +1,15 @@
+package com.iiatimd.portfolioappv2.Network;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+import com.iiatimd.portfolioappv2.Entities.AccessToken;
+
+public interface ApiService {
+
+    @POST("register")
+    @FormUrlEncoded
+    Call<AccessToken> register(@Field("email") String email, @Field("password") String password);
+
+}
