b96b340b954271f28cb6d84702ef5aaeb263093d
Klaas Schoute <klaas_schoute@hotmail.com>
2020-08-18 11:36:24 +0200

    ✨ Registratie activity (#1)
    * :sparkles: Registratie layout en de activity code
    
    * :hammer: Tool - debuggen in Chrome console
    
    * :sparkles: Opslaan van Oauth tokens
    
    * :sparkles: Api call met error handling

---
+++ app/src/main/java/com/iiatimd/portfolioappv2/Utils.java
@@ -0,0 +1,26 @@
+package com.iiatimd.portfolioappv2;
+
+import com.iiatimd.portfolioappv2.Entities.ApiError;
+import com.iiatimd.portfolioappv2.Network.RetrofitBuilder;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+public class Utils {
+
+    public static ApiError converErrors(ResponseBody response) {
+        Converter<ResponseBody, ApiError> converter = RetrofitBuilder.getRetrofit().responseBodyConverter(ApiError.class, new Annotation[0]);
+
+        ApiError apiError = null;
+
+        try {
+            apiError = converter.convert(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return apiError;
+    }
+}
